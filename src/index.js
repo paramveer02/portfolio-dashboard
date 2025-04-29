fbd1cb0fc20139b1952629d2000e1d1d7fb0e8ac
Paramveer <paramveermarwahafc@gmail.com>
2025-04-29 13:55:41 +0200

    log welcome card

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { userGreeting, getUserInitials } from "./utils/helper.js";
-import { introduceUser } from "./services/userService.js";
+import { introduceUser, userWelcomeCard } from "./services/userService.js";
 
 console.log(userGreeting('Paramvir'));
 console.log(userGreeting());
@@ -10,3 +10,6 @@ console.log(getUserInitials());
 
 console.log(introduceUser('Arjun Sharad'));
 console.log(introduceUser('Arjun'));
+
+console.log(userWelcomeCard('Sapna Dave'));
+console.log(userWelcomeCard());
